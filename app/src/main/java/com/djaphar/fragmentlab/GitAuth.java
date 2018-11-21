c2394222aa45e930a1bbc8834829a281dae3fe3d
MetalUpYourAss <djapar-shah@mail.ru>
2018-11-21 21:34:53 +0300

    Подключение к api github

--- app/src/main/java/com/djaphar/fragmentlab/GitAuth.java
+++ app/src/main/java/com/djaphar/fragmentlab/GitAuth.java
@@ -1,31 +1,112 @@
 package com.djaphar.fragmentlab;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class GitAuth extends Fragment{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class GitAuth extends Fragment {
+
+    TextView textView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              final Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_git_auth, container, false);
 
-        final Fragment fragment = new GitRepoFragment();
-        Button button = rootView.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        final EditText editText = rootView.findViewById(R.id.editText2);
+        textView = rootView.findViewById(R.id.textView3);
+        rootView.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "kekekeke", Toast.LENGTH_SHORT).show();
-                getFragmentManager().beginTransaction().replace(R.id.main_fragment, fragment).commit();
+                if (!editText.getText().toString().equals("")) {
+                    new GitConnectionTask().execute("https://api.github.com/users/" +
+                                                            editText.getText().toString() + "/repos");
+                } else {
+                    Toast.makeText(getActivity(), "Введите имя пользователя!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         return rootView;
+    }
+
+    class GitConnectionTask extends AsyncTask<String, Void, String> {
+
+        String answerHTTP;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... newUrl) {
+            HttpsURLConnection connection = null;
+            try {
+                URL url = new URL(newUrl[0]);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setReadTimeout(10000);
+                connection.connect();
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                answerHTTP = streamConverse(connection.getInputStream());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            textView.setText(answerHTTP);
+        }
+    }
+
+    public String streamConverse(InputStream in) {
+        BufferedReader reader = null;
+        StringBuilder response = new StringBuilder();
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return response.toString();
     }
 }
--- app/src/main/java/com/djaphar/fragmentlab/MainActivity.java
+++ app/src/main/java/com/djaphar/fragmentlab/MainActivity.java
@@ -4,10 +4,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,15 +14,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    Fragment gitAuthFragment, gitRepoFragment, mapsFragment;
+    Fragment gitAuthFragment, mapsFragment;
     Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
@@ -48,10 +49,9 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        gitAuthFragment = new GitAuth();
-        gitRepoFragment = new GitRepoFragment();
-        mapsFragment = new MapsFragment();
 
+        gitAuthFragment = new GitAuth();
+        mapsFragment = new MapsFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.main_fragment, gitAuthFragment).commit();
     }
 
