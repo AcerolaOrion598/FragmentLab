82fc8653f194cc21196f925f8f99a33cf5b4959d
MetalUpYourAss <djapar-shah@mail.ru>
2019-01-07 18:57:30 +0300

    Готовая камера + заряд батареи

--- app/src/main/java/com/djaphar/fragmentlab/SensorAndCameraFragment.java
+++ app/src/main/java/com/djaphar/fragmentlab/SensorAndCameraFragment.java
@@ -1,10 +1,9 @@
 package com.djaphar.fragmentlab;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,10 +17,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
@@ -29,13 +27,13 @@ import java.util.Objects;
 
 public class SensorAndCameraFragment extends Fragment {
 
-    MainActivity mainActivity;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_SAVE_PHOTO = 2;
-    ImageView testImageView;
+    static final int REQUEST_IMAGE_CAPTURE_AND_SAVE = 1;
     private static final int PERMISSION_REQUEST_CODE = 123;
-    Button takePictureButton, savePictureButton;
-    String currentPhotoPath;
+    MainActivity mainActivity;
+    ImageView testImageView;
+    TextView batteryTV;
+    Button takePictureButton;
+    Uri outputFileUri;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -43,8 +41,8 @@ public class SensorAndCameraFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_sensor_and_camera, container, false);
         mainActivity = (MainActivity) getActivity();
         testImageView = rootView.findViewById(R.id.testImageView);
+        batteryTV = rootView.findViewById(R.id.batteryTV);
         takePictureButton = rootView.findViewById(R.id.takePictureButton);
-        savePictureButton = rootView.findViewById(R.id.savePictureButton);
         return rootView;
     }
 
@@ -55,43 +53,30 @@ public class SensorAndCameraFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if (hasPermissions()) {
-                    takePictureIntent();
+                    takeAndSavePictureIntent();
                 } else {
                     requestPerms();
                 }
             }
         });
 
-        savePictureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                savePictureIntent();
-            }
-        });
+        IntentFilter batteryFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = mainActivity.registerReceiver(null, batteryFilter);
+        String status = "Заряд батареи: " + Integer.toString(Objects.requireNonNull(batteryStatus)
+                                                            .getIntExtra("level", -1)) + "%";
+        batteryTV.setText(status);
     }
 
-    private void takePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(mainActivity.getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
+    private void takeAndSavePictureIntent() {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDirectory =  new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                                                                                                imageFileName + ".jpg");
+        outputFileUri = Uri.fromFile(storageDirectory);
 
-    private void savePictureIntent() {
-        Intent savePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (savePictureIntent.resolveActivity(mainActivity.getPackageManager()) != null) {
-            File photo = null;
-            try {
-                photo = createImageFile();
-            } catch (IOException e) {
-                Toast.makeText(this.getContext(), "Не удалось сохранить фото", Toast.LENGTH_SHORT).show();
-            }
-
-            if (photo != null) {
-                savePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
-                startActivityForResult(savePictureIntent, REQUEST_SAVE_PHOTO);
-            }
-        }
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE_AND_SAVE);
     }
 
     private boolean hasPermissions() {
@@ -117,36 +102,11 @@ public class SensorAndCameraFragment extends Fragment {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        takePictureIntent();
+        takeAndSavePictureIntent();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            Bundle extras = data.getExtras();
-            if (extras != null) {
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                testImageView.setImageBitmap(imageBitmap);
-                savePictureButton.setEnabled(true);
-            }
-        } else if (requestCode == REQUEST_SAVE_PHOTO && resultCode == Activity.RESULT_OK) {
-            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-            File f = new File(currentPhotoPath);
-            Uri contentUri = Uri.fromFile(f);
-            mediaScanIntent.setData(contentUri);
-            mainActivity.sendBroadcast(mediaScanIntent);
-            Toast.makeText(this.getContext(), "Фото сохранено в галерею", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-
-        File image = File.createTempFile(imageFileName, ".jpg", storageDirectory);
-        currentPhotoPath = "file:" + image.getAbsolutePath();
-
-        return image;
+        testImageView.setImageURI(outputFileUri);
     }
 }
