75fb5b2635a4e0922070befa222a25bb6fb352f7
MetalUpYourAss <djapar-shah@mail.ru>
2019-01-08 17:52:42 +0300

    Полностью готовые камера и датчик

--- app/src/main/java/com/djaphar/fragmentlab/SensorAndCameraFragment.java
+++ app/src/main/java/com/djaphar/fragmentlab/SensorAndCameraFragment.java
@@ -1,9 +1,13 @@
 package com.djaphar.fragmentlab;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,11 +22,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class SensorAndCameraFragment extends Fragment {
@@ -31,9 +38,18 @@ public class SensorAndCameraFragment extends Fragment {
     private static final int PERMISSION_REQUEST_CODE = 123;
     MainActivity mainActivity;
     ImageView testImageView;
-    TextView batteryTV;
-    Button takePictureButton;
+    TextView sensorTV;
+    Button takePictureButton, accelButton;
     Uri outputFileUri;
+    SensorManager sensorManager;
+    Sensor sensorAccel;
+    Timer timer;
+    TimerTask task;
+
+    StringBuilder stringBuilder = new StringBuilder();
+    float[] valuesAccel = new float[3];
+//    float[] valuesAccelGravity = new float[3];
+//    float[] valuesAccelMotion = new float[3];
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -41,8 +57,9 @@ public class SensorAndCameraFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_sensor_and_camera, container, false);
         mainActivity = (MainActivity) getActivity();
         testImageView = rootView.findViewById(R.id.testImageView);
-        batteryTV = rootView.findViewById(R.id.batteryTV);
+        sensorTV = rootView.findViewById(R.id.sensorTV);
         takePictureButton = rootView.findViewById(R.id.takePictureButton);
+        accelButton = rootView.findViewById(R.id.accelButton);
         return rootView;
     }
 
@@ -60,11 +77,46 @@ public class SensorAndCameraFragment extends Fragment {
             }
         });
 
-        IntentFilter batteryFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = mainActivity.registerReceiver(null, batteryFilter);
-        String status = "Заряд батареи: " + Integer.toString(Objects.requireNonNull(batteryStatus)
-                                                            .getIntExtra("level", -1)) + "%";
-        batteryTV.setText(status);
+        sensorManager = (SensorManager) mainActivity.getSystemService(Context.SENSOR_SERVICE);
+        sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        accelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (accelButton.getText().toString().equals(getString(R.string.button_accel_on))) {
+                    accelButton.setText(getString(R.string.button_accel_off));
+                    sensorManager.registerListener(listener, sensorAccel, SensorManager.SENSOR_DELAY_UI);
+                    timer = new Timer();
+                    task = new MyTimerTask();
+                    timer.schedule(task, 0, 250);
+                } else {
+                    accelButton.setText(getString(R.string.button_accel_on));
+                    sensorTV.setText(getString(R.string.accel_text_view));
+                    sensorManager.unregisterListener(listener);
+                    timer.cancel();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (accelButton.getText().toString().equals(getString(R.string.button_accel_off))) {
+            sensorManager.unregisterListener(listener);
+            timer.cancel();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (accelButton.getText().toString().equals(getString(R.string.button_accel_off))) {
+            sensorManager.registerListener(listener, sensorAccel, SensorManager.SENSOR_DELAY_UI);
+            task = new MyTimerTask();
+            timer = new Timer();
+            timer.schedule(task, 0, 250);
+        }
     }
 
     private void takeAndSavePictureIntent() {
@@ -108,5 +160,51 @@ public class SensorAndCameraFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         testImageView.setImageURI(outputFileUri);
+        Toast.makeText(this.getContext(), getString(R.string.toast_picture_save), Toast.LENGTH_SHORT).show();
+    }
+
+    private void showInfo() {
+        stringBuilder.setLength(0);
+
+//        stringBuilder.append("Accel: ").append(format(valuesAccel))
+//                .append("\nAccel motion: ").append(format(valuesAccelMotion))
+//                .append("\nAccel gravity: ").append(format(valuesAccelGravity));
+
+        stringBuilder.append("Accel: ").append(format(valuesAccel));
+        sensorTV.setText(stringBuilder);
+    }
+
+    private String format(float[] values) {
+        return String.format("%1$.1f\t\t%2$.1f\t\t%3$.1f", values[0], values[1], values[2]);
+    }
+
+    SensorEventListener listener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+//            for (int i = 0; i < 3; i++) {
+//                valuesAccel[i] = sensorEvent.values[i];
+//                valuesAccelGravity[i] = (float) (0.1 * sensorEvent.values[i] + 0.9 * valuesAccelGravity[i]);
+//                valuesAccelMotion[i] = sensorEvent.values[i] - valuesAccelGravity[i];
+//            }
+
+            System.arraycopy(sensorEvent.values, 0, valuesAccel, 0, 3);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) { }
+    };
+
+    class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            mainActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    showInfo();
+                }
+            });
+        }
     }
 }
